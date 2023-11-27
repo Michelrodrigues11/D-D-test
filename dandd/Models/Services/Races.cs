56a20eb449cb715fad28947e5019929509964d53
Michel <henriquemichel10@gmail.com>
2023-11-27 20:53:18 -0300

    Não foi

---
+++ dandd/Models/Services/Races.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace dandd.Models.Services
+{
+    public class Races
+    {
+        private HttpClient client;
+        private JsonSerializerOptions _serializerOptions;
+        private const string baseUrl = "https://www.dnd5eapi.co/api/ability-scores/cha";
+
+        public Races() { }
+
+
+    }
+}
---
+++ dandd/ViewModels/RacesViewModel.cs
@@ -0,0 +1,51 @@
+﻿using dandd.Models.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using dandd.Services;
+
+namespace dandd.ViewModels
+{
+    internal partial class RacesViewModel : ObservableObject, IDisposable
+    {
+    
+
+
+        private readonly RacesService _racesService;
+
+        [ObservableProperty]
+        public int _UserId;
+        [ObservableProperty]
+        public int _Id;
+        [ObservableProperty]
+        public string _Title;
+        [ObservableProperty]
+        public string _Body;
+        //Uma coleção de Post
+        [ObservableProperty]
+        public ObservableCollection<Races> _races;
+
+        public RacesViewModel()
+        {
+
+            Races = new ObservableCollection<Races>();
+            _racesService = new RacesService();
+        }
+        public ICommand GetPostsCommand => new Command(async () => await LoadPostAsync());
+
+        private async Task LoadPostAsync()
+        {
+            RacesViewModel = await _racesService.GetPostsAsync();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
